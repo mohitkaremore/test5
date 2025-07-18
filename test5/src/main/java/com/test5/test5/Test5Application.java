41d332d794296fc728675d228e8859dbe0fd51f5
mohitkaremore4@gmail.com <mohitkaremore4@gmail.com>
2025-07-18 10:45:40 +0530

    new features

--- test5/src/main/java/com/test5/test5/Test5Application.java
+++ test5/src/main/java/com/test5/test5/Test5Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test5Application {
+	int =200;
+	int=400;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
