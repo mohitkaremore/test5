2064a769952432afd27765438060865176b8e268
mohitkaremore <mohitkaremore4@gmail.com>
2025-07-19 09:22:48 +0530

    Update Test5Application.java

--- test5/src/main/java/com/test5/test5/Test5Application.java
+++ test5/src/main/java/com/test5/test5/Test5Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test5Application {
-
+               int x=20;
+	int y=30;
+	int z=30;
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
 	}
